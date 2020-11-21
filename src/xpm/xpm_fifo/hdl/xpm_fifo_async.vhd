e6f52bd9cfe2e25e32b71bcaa6380012cd226645
Frans Schreuder <fransschreuder@gmail.com>
2020-11-21 17:46:22 +0100

    Translated xpm_fifo_async to vhdl

--- src/xpm/xpm_fifo/hdl/xpm_fifo_async.vhd
+++ src/xpm/xpm_fifo/hdl/xpm_fifo_async.vhd
@@ -3,26 +3,28 @@ use ieee.std_logic_1164.all;
 library std;
 use std.env.all;
 
-entity xpm_fifo_sync is
+entity xpm_fifo_async is
   generic (
 
     -- Common module generics
     FIFO_MEMORY_TYPE         : string   := "auto";
     FIFO_WRITE_DEPTH         : integer  := 2048;
     CASCADE_HEIGHT           : integer  := 0;
+    RELATED_CLOCKS           : integer  := 0;
     WRITE_DATA_WIDTH         : integer  := 32;
     READ_MODE                : string   :="std";
     FIFO_READ_LATENCY        : integer  := 1;
     FULL_RESET_VALUE         : integer  := 0;
     USE_ADV_FEATURES         : string   :="0707";
     READ_DATA_WIDTH          : integer  := 32;
+    CDC_SYNC_STAGES          : integer  := 2;
     WR_DATA_COUNT_WIDTH      : integer  := 1;
     PROG_FULL_THRESH         : integer  := 10;
     RD_DATA_COUNT_WIDTH      : integer  := 1;
     PROG_EMPTY_THRESH        : integer  := 10;
     DOUT_RESET_VALUE         : string   := "0";
     ECC_MODE                 : string   :="no_ecc";
-    SIM_ASSERT_CHK           : integer  := 0;
+    SIM_ASSERT_CHK           : integer := 0    ;
     WAKEUP_TIME              : integer  := 0
   );
   port (
@@ -39,6 +41,7 @@ entity xpm_fifo_sync is
     wr_rst_busy    : out std_logic;
     almost_full    : out std_logic;
     wr_ack         : out std_logic;
+    rd_clk         : in std_logic;
     rd_en          : in std_logic;
     dout           : out std_logic_vector(READ_DATA_WIDTH-1 downto 0);
     empty          : out std_logic;
@@ -53,12 +56,11 @@ entity xpm_fifo_sync is
     sbiterr        : out std_logic;
     dbiterr        : out std_logic
   );
-end xpm_fifo_sync;
+end xpm_fifo_async;
 
+architecture rtl of xpm_fifo_async is
 
-architecture rtl of xpm_fifo_sync is
-  
-  -- Function to convert ASCII value to binary 
+-- Function to convert ASCII value to binary 
   function str2bin(str_val_ascii: character) return std_logic_vector is
   begin
     case(str_val_ascii) is
@@ -95,9 +97,10 @@ architecture rtl of xpm_fifo_sync is
     return rst_val_conv_a_i(15 downto 0);
   end function;
 
-  constant EN_ADV_FEATURE_SYNC : std_logic_vector(15 downto 0) := hstr2bin(USE_ADV_FEATURES);
+  constant EN_ADV_FEATURE_ASYNC : std_logic_vector(15 downto 0) := hstr2bin(USE_ADV_FEATURES);
 
-  function F_FIFO_MEMORY_TYPE(T : string) return integer is
+  
+function F_FIFO_MEMORY_TYPE(T : string) return integer is
   begin
     
     if (T = "lutram"   or T = "LUTRAM"   or T = "distributed"   or T = "DISTRIBUTED"  ) then
@@ -116,9 +119,9 @@ architecture rtl of xpm_fifo_sync is
   -- Define local parameters for mapping with base file
   constant P_FIFO_MEMORY_TYPE      : integer := F_FIFO_MEMORY_TYPE(FIFO_MEMORY_TYPE);
   
-  constant P_COMMON_CLOCK : integer := 1;
+  constant  P_COMMON_CLOCK  : integer := 0;
 
-  function F_ECC_MODE(M: string) return integer is
+function F_ECC_MODE(M: string) return integer is
   begin
     if M = "no_ecc" or M = "NO_ECC" then
         return 0;
@@ -152,14 +155,14 @@ architecture rtl of xpm_fifo_sync is
   end function;
   
   constant P_WAKEUP_TIME : integer := WAKEUP_TIME;
-  signal wr_rst_busy_s: std_logic;
+
 begin
-  
+
   process
     variable drc_err_flag_sync : integer := 0;
   begin
-    if (EN_ADV_FEATURE_SYNC(13) /= '0') then
-      report("(XPM_FIFO_SYNC 1-1) USE_ADV_FEATURES(13) = "&to_string(EN_ADV_FEATURE_SYNC(13))&". This is a reserved field and must be set to 0.") severity error;
+    if (EN_ADV_FEATURE_ASYNC(13) /= '0') then
+      report("(XPM_FIFO_ASYNC 1-1) USE_ADV_FEATURES(13) = "&to_string(EN_ADV_FEATURE_ASYNC(13))&". This is a reserved field and must be set to 0.") severity error;
       drc_err_flag_sync := 1;
     end if;
   
@@ -171,60 +174,59 @@ begin
   -- -------------------------------------------------------------------------------------------------------------------
   -- Generate the instantiation of the appropriate XPM module
   -- -------------------------------------------------------------------------------------------------------------------
-       rd_rst_busy <= wr_rst_busy_s;
-       wr_rst_busy <= wr_rst_busy_s;
-       
-      xpm_fifo_base_inst: entity work.xpm_fifo_base 
-      generic map (
-        COMMON_CLOCK               => P_COMMON_CLOCK      ,
-        FIFO_MEMORY_TYPE           => P_FIFO_MEMORY_TYPE  ,
-        ECC_MODE                   => P_ECC_MODE          ,
-        SIM_ASSERT_CHK             => SIM_ASSERT_CHK      ,
-        CASCADE_HEIGHT             => CASCADE_HEIGHT      ,
-        FIFO_WRITE_DEPTH           => FIFO_WRITE_DEPTH    ,
-        WRITE_DATA_WIDTH           => WRITE_DATA_WIDTH    ,
-        WR_DATA_COUNT_WIDTH        => WR_DATA_COUNT_WIDTH ,
-        PROG_FULL_THRESH           => PROG_FULL_THRESH    ,
-        FULL_RESET_VALUE           => FULL_RESET_VALUE    ,
-        USE_ADV_FEATURES           => USE_ADV_FEATURES    ,
-        READ_MODE                  => P_READ_MODE         ,
-        FIFO_READ_LATENCY          => FIFO_READ_LATENCY   ,
-        READ_DATA_WIDTH            => READ_DATA_WIDTH     ,
-        RD_DATA_COUNT_WIDTH        => RD_DATA_COUNT_WIDTH ,
-        PROG_EMPTY_THRESH          => PROG_EMPTY_THRESH   ,
-        DOUT_RESET_VALUE           => DOUT_RESET_VALUE    ,
-        CDC_DEST_SYNC_FF           => 2                   ,
-        REMOVE_WR_RD_PROT_LOGIC    => 0                   ,
-        WAKEUP_TIME                => WAKEUP_TIME               
-      )  
-      port map(
-        sleep            => sleep,
-        rst              => rst,
-        wr_clk           => wr_clk,
-        wr_en            => wr_en,
-        din              => din,
-        full             => full,
-        full_n           => open,
-        prog_full        => prog_full,
-        wr_data_count    => wr_data_count,
-        overflow         => overflow,
-        wr_rst_busy      => wr_rst_busy_s,
-        almost_full      => almost_full,
-        wr_ack           => wr_ack,
-        rd_clk           => wr_clk,
-        rd_en            => rd_en,
-        dout             => dout,
-        empty            => empty,
-        prog_empty       => prog_empty,
-        rd_data_count    => rd_data_count,
-        underflow        => underflow,
-        rd_rst_busy      => open,
-        almost_empty     => almost_empty,
-        data_valid       => data_valid,
-        injectsbiterr    => injectsbiterr,
-        injectdbiterr    => injectdbiterr,
-        sbiterr          => sbiterr,
-        dbiterr          => dbiterr
-      );
-
+      gnuram_async_fifo: if (P_FIFO_MEMORY_TYPE /= 3) generate
+        xpm_fifo_base_inst: entity work.xpm_fifo_base 
+        generic map (
+          COMMON_CLOCK               => P_COMMON_CLOCK      ,
+          RELATED_CLOCKS             => RELATED_CLOCKS      ,
+          FIFO_MEMORY_TYPE           => P_FIFO_MEMORY_TYPE  ,
+          ECC_MODE                   => P_ECC_MODE          ,
+          SIM_ASSERT_CHK             => SIM_ASSERT_CHK      ,
+          CASCADE_HEIGHT             => CASCADE_HEIGHT      ,
+          FIFO_WRITE_DEPTH           => FIFO_WRITE_DEPTH    ,
+          WRITE_DATA_WIDTH           => WRITE_DATA_WIDTH    ,
+          WR_DATA_COUNT_WIDTH        => WR_DATA_COUNT_WIDTH ,
+          PROG_FULL_THRESH           => PROG_FULL_THRESH    ,
+          FULL_RESET_VALUE           => FULL_RESET_VALUE    ,
+          USE_ADV_FEATURES           => USE_ADV_FEATURES    ,
+          READ_MODE                  => P_READ_MODE         ,
+          FIFO_READ_LATENCY          => FIFO_READ_LATENCY   ,
+          READ_DATA_WIDTH            => READ_DATA_WIDTH     ,
+          RD_DATA_COUNT_WIDTH        => RD_DATA_COUNT_WIDTH ,
+          PROG_EMPTY_THRESH          => PROG_EMPTY_THRESH   ,
+          DOUT_RESET_VALUE           => DOUT_RESET_VALUE    ,
+          CDC_DEST_SYNC_FF           => CDC_SYNC_STAGES     ,
+          REMOVE_WR_RD_PROT_LOGIC    => 0                   ,
+          WAKEUP_TIME                => WAKEUP_TIME         
+        )  
+        port map(
+          sleep            => sleep,
+          rst              => rst,
+          wr_clk           => wr_clk,
+          wr_en            => wr_en,
+          din              => din,
+          full             => full,
+          full_n           => open,
+          prog_full        => prog_full,
+          wr_data_count    => wr_data_count,
+          overflow         => overflow,
+          wr_rst_busy      => wr_rst_busy,
+          almost_full      => almost_full,
+          wr_ack           => wr_ack,
+          rd_clk           => rd_clk,
+          rd_en            => rd_en,
+          dout             => dout,
+          empty            => empty,
+          prog_empty       => prog_empty,
+          rd_data_count    => rd_data_count,
+          underflow        => underflow,
+          rd_rst_busy      => rd_rst_busy,
+          almost_empty     => almost_empty,
+          data_valid       => data_valid,
+          injectsbiterr    => injectsbiterr,
+          injectdbiterr    => injectdbiterr,
+          sbiterr          => sbiterr,
+          dbiterr          => dbiterr
+        );
+      end generate gnuram_async_fifo;
 end rtl;
